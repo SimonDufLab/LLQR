8eafb826900566c16d4c533c39573d7d373faadc
Simon Dufort-Labbé <sduf.lab@gmail.com>
2025-04-30 21:04:39 -0400

    Corrected mistake in divergence fn expression for ngd (was not accounting for log-softmax despite old comment...)

--- lqr_optimizer/_src/utils/divergence.py
+++ lqr_optimizer/_src/utils/divergence.py
@@ -4,5 +4,6 @@ import jax.numpy as jnp
 
 # Divergence function (for NGD):
 def ngd_divergence_f(px, px_):
-  # Taking into account we return log-softmax
-  return (-px * jnp.log(px_)).sum()
+  # Taking into account that we return log-softmax and simplifying since we are interested in the second derivative
+  # return (-px * jnp.log(px_)).sum() This is when we return softmax
+  return (-jnp.exp(px) * px_).sum()
